6bb3a767afea9563e0f26467c32166858840f961
David-Jhun <davidjhun266@gmail.com>
2020-04-03 19:36:51 -0500

    The method to add an employee was implemented.

--- src/model/Company.java
+++ src/model/Company.java
@@ -11,5 +11,46 @@ public class Company {
 	public Employee getRoot() {
 		return root;
 	}
+	
+	public void add( String id, String name, int office ) {
+		Employee newOne = new Employee( id, name, office );
+		if( root == null ) {
+			root = newOne;
+		}else if( root.compareTo(newOne) < 0 ){
+			if( root.getLeftSon() == null ) {
+				root.setLeftSon(newOne);
+				newOne.setFather(root);
+			}else {
+				add(root.getLeftSon(), newOne);
+			}
+		}else {
+			if( root.getRightSon() == null ) {
+				root.setRightSon(newOne);
+				newOne.setFather(root);
+			}else {
+				add(root.getRightSon(), newOne);
+			}
+		}
+	}
+	
+	private void add( Employee currentRoot, Employee newOne ) {
+		if( currentRoot == null ) {
+			currentRoot = newOne;
+		}else if( currentRoot.compareTo(newOne) < 0 ) {
+			if( currentRoot.getLeftSon() == null ) {
+				currentRoot.setLeftSon(newOne);
+				newOne.setFather(currentRoot);
+			}else {
+				add(currentRoot.getLeftSon(), newOne);
+			}
+		}else {
+			if( currentRoot.getRightSon() == null ) {
+				currentRoot.setRightSon(newOne);
+				newOne.setFather(currentRoot);
+			}else {
+				add(currentRoot.getRightSon(), newOne);
+			}
+		}
+	}
 
 }
